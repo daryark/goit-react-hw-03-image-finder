0a4686be5bf54b15941601961fec340d299c156b
Dasha Yarkovska <yarkovska11@gmail.com>
2023-02-16 21:33:02 +0200

    just tidy up

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  z-index: 1500;
+  z-index: 1500; //чтобы перекрыть хедер с з-индексом 999
+
   display: flex;
   position: fixed;
+
   width: 100vw;
   height: 100vh;
   top: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.6);
+
   justify-content: center;
   align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.6);
 `;
